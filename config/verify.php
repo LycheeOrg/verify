81af073192cff27ff2964295947927edfa91bd82
ildyria <benoit@viguier.nl>
2024-09-27 17:26:06 +0200

    add quick checks

--- config/verify.php
+++ config/verify.php
@@ -10,7 +10,7 @@ return [
 	'validation' => [
 		ValidateHash::class => 'c34da4a4523e54f303c23cd22eaf252f74ed7965',
 		ValidateSignature::class => 'a3d4081247b4f56c8aeb7c6b192415700e27acc0',
-		Verify::class => 'cda79b50522e9189aa928a5f471ebc20a049db76',
+		Verify::class => 'd186e2502a0b89e1713ce4d43d1c69ff5e7c311c',
 		VerifySupporterStatus::class => '6358c45ed0414c1e2697e0881238659fa6221bed',
 		VerifyServiceProvider::class => '927a8f3c811fc82cb8a0ac2667c06e7d292c3633',
 	],
