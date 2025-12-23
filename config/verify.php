be8b436cba7fef4ba4328c46c954c235d4edb0ea
Benoît Viguier <ildyria@users.noreply.github.com>
2025-12-23 10:01:17 +0100

    Memoize the result to avoid recomputing the hash every time (#7)

--- config/verify.php
+++ config/verify.php
@@ -14,7 +14,7 @@ return [
 		ValidateSupporter::class => 'ef1a42701af6dc36e052556a0ee1c762394f9428',
 		ValidatePro::class => '482b48f1a026684b6c1754e45ca180ffc52483ff',
 		ValidateSignature::class => '5a8a855d4b59c44c298daa66801c79f2aba20492',
-		Verify::class => 'f15a25c69847bf635c7b0cc730ea24a5aaf008b8',
+		Verify::class => '421e83a049b1df58c73b546adb132a082075fd2c',
 		VerifySupporterStatus::class => '6358c45ed0414c1e2697e0881238659fa6221bed',
 		VerifyProStatus::class => '212e6ada794587ee8e2b81cf76e243d134a7e823',
 		VerifyServiceProvider::class => '923b63b15d25e69b95ed1d5ec1c82ba57f1a7d74',
